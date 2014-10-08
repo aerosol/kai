512724a5d8b796cd9385f7fa9151bde8ba9ed148
Adam Rutkowski <hq@mtod.org>
2014-10-08 11:49:27 +0200

    Use random metric names in tests

--- test/rest_SUITE.erl
+++ test/rest_SUITE.erl
@@ -3,11 +3,15 @@
          all/0,
          init_per_suite/1,
          end_per_suite/1,
+         init_per_testcase/2,
+         end_per_testcase/2,
          t_simple_query_metric/1,
          t_put_and_get_metric/1]).
 
 -define(q, kai_q).
 -define(th, test_helpers).
+
+-include_lib("common_test/include/ct.hrl").
 
 suite() ->
     [{timetrap,{seconds,30}}].
@@ -15,6 +19,7 @@ suite() ->
 all() -> ?th:all_tests(?MODULE).
 
 init_per_suite(Config) ->
+    random:seed(),
     Config2 = ?th:set_common_data_dir(?MODULE, Config),
     {ok, _} = kai:start(),
     case kai:kairosdb_version() of
@@ -27,35 +32,48 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
     ok.
 
-t_simple_query_metric(_) ->
+init_per_testcase(_TestCase, Config) ->
+    MetricName = << <<($z-random:uniform(24))>> || _ <- lists:seq(1, 20) >>,
+    {ok, _} = kai_rest:delete_metric(MetricName),
+    kvlists:set_value(metric_name, MetricName, Config).
+
+end_per_testcase(_TestCase, Config) ->
+    {ok, _} = kai_rest:delete_metric(mname(Config)),
+    ok.
+
+mname(Config) ->
+    ?config(metric_name, Config).
+
+t_simple_query_metric(Config) ->
+    MN = mname(Config),
     Q1 = ?q:new({5, days}),
-    M0 = ?q:metric(<<"metric_up_in_dis">>, 666),
+    M0 = ?q:metric(MN),
     Q2 = ?q:compose(Q1, [M0]),
     case kai_rest:query_metrics(Q2) of
         {ok,[{<<"queries">>,
               [[{<<"sample_size">>,0},
                 {<<"results">>,
-                 [[{<<"name">>,<<"metric_up_in_dis">>},
+                 [[{<<"name">>,MN},
                    {<<"tags">>,[{}]},
                    {<<"values">>,[]}]]}]]}]}  ->
-            {ok, no_content} = kai_rest:delete_datapoints(Q2),
             ok;
         {error, {kairosdb, R}} ->
             {skip, R}
     end.
 
-t_put_and_get_metric(_) ->
+t_put_and_get_metric(Config) ->
+    MN = mname(Config),
     Q1 = ?q:new({5, days}),
-    M0 = ?q:metric(<<"you_da_metric">>, 666),
+    M0 = ?q:metric(mname(Config)),
     Q2 = ?q:compose(Q1, [M0]),
-    case kai:put_metric(<<"you_da_metric">>, 666) of
+    case kai:put_metric(MN, 666) of
         ok ->
+            timer:sleep(1500), %% kairosdb buffer
             {ok, Results} = kai_rest:query_metrics(Q2),
-            {ok, no_content} = kai_rest:delete_datapoints(Q2),
             [{<<"queries">>,
               [[{<<"sample_size">>,1},
                 {<<"results">>,
-                 [[{<<"name">>,<<"you_da_metric">>},
+                 [[{<<"name">>,MN},
                    {<<"group_by">>,
                     [[{<<"name">>,<<"type">>},{<<"type">>,<<"number">>}]]},
                    {<<"tags">>,[{<<"add">>,[<<"tag">>]}]},
