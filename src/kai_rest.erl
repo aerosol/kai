67eee47f80659770b8641ac25a7202a45e9b2b83
Adam Rutkowski <hq@mtod.org>
2014-11-15 21:56:38 +0100

    Add folsom stats (first pass)

--- src/kai_rest.erl
+++ src/kai_rest.erl
@@ -49,18 +49,33 @@ call(ApiMethod, HttpMethod, Payload)
 call(ApiMethod, HttpMethod, Suffix, Payload) ->
     URI = uri(ApiMethod, Suffix),
     HandleReply = reply_handler(ApiMethod, HttpMethod),
-    case request(HttpMethod, URI, Payload) of
-        {error, R} ->
-            {error, {kairosdb, R}};
-        {ok, {{Status, _}, _, P}} ->
-            case lists:member(Status, [200, 201, 202, 204]) of
-                true ->
-                    RespPayload = decode(P),
-                    HandleReply(RespPayload);
-                false ->
-                    {error, {kairosdb, {Status, P}}}
-            end
-    end.
+
+    LatOK  = kai_folsom:begin_rest_ok_lat(ApiMethod),
+    LatNOK = kai_folsom:begin_rest_nok_lat(ApiMethod),
+
+    SpiOK  = kai_folsom:name_rest_ok(ApiMethod),
+    SpiNOK = kai_folsom:name_rest_nok(ApiMethod),
+
+    Result = case request(HttpMethod, URI, Payload) of
+                 {error, R} ->
+                     kai_folsom:notify_lat(LatNOK),
+                     kai_folsom:notify_spiral(SpiNOK),
+                     {error, {kairosdb, R}};
+                 {ok, {{Status, _}, _, P}} ->
+                     case lists:member(Status, [200, 201, 202, 204]) of
+                         true ->
+                             RespPayload = decode(P),
+                             Reply = HandleReply(RespPayload),
+                             kai_folsom:notify_lat(LatOK),
+                             kai_folsom:notify_spiral(SpiOK),
+                             Reply;
+                         false ->
+                             kai_folsom:lat_notify(LatNOK),
+                             kai_folsom:notify_spiral(SpiNOK),
+                             {error, {kairosdb, {Status, P}}}
+                     end
+             end,
+    Result.
 
 
 %% Internals
